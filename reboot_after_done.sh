411e911f4055f349f2c3c4b5673e74be77a3f941
Yusuke Izawa <me@yizawa.com>
2024-01-03 13:46:09 +0900

    Fix reboot_after_done.sh

--- reboot_after_done.sh
+++ reboot_after_done.sh
@@ -21,14 +21,15 @@ create_or_incr_file() {
 do_command() {
     create_or_incr_file $count && sleep 0.1
 
-    if [ ! -f $f ] || [ $(head -n 1 $count) -le $max_invocation ]; then
+    if [ ! -f $f ] || [ $(head -n 1 $count) -lt $max_invocation ]; then
         echo "Do command..." && sleep 0.1
         cd $bm_path || exit 1
         ./rebench -in 1 -c runbench.conf
         cat $count > $done
         reboot
     else
-        echo "Max invocation count exceeded." > $done && sleep 0.1
+        echo "Max invocation count exceeded."
+        echo "Done." > $done && sleep 0.1
     fi
 }
 
